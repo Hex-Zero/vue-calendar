771ea5c6ed691b10594fc9a4db3e2b680e2406f5
Ace Alrich <hex0@live.com>
2021-02-02 20:11:20 +0000

    new begging

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ greeting }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      greeting: "OI"
+    }
   }
 }
 </script>
